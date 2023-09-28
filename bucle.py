0c443bc0842cfb539fc546dc55c6c4080a139de7
Moises6636 <moisees6636@gmail.com>
2023-09-28 18:32:03 -0400

    ejercicio listo

--- bucle.py
+++ bucle.py
@@ -1,1 +1,10 @@
+import readchar
+print("Presiona la tecla ↑ para salir del bucle")
+while True:
+    key = readchar.readkey()
+    print(f"Tecla presionada: {key}")
+    
+    if key == "up":
+        break
+
 
